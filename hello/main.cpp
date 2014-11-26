983e673e7928809818a0799741a05a2a50339248
louis <louis@louisdeMacBook-Air.local>
2014-11-27 00:14:34 +0800

    Initial Commit

---
+++ hello/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello
+//
+//  Created by louis on 14/11/27.
+//  Copyright (c) 2014年 louis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
