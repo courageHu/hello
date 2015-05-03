efee8a98ef348a4f894b61cc3be2171fec2b9f61
louis <louis@louisdeMacBook-Air.local>
2015-05-03 23:17:27 +0800

    Initial Commit

---
+++ code/the first c project/the first c project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  the first c project
+//
+//  Created by louis on 15/5/3.
+//  Copyright (c) 2015年 louis. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
